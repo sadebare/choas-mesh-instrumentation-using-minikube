bb693c4c48da28266649d9b46fd9ab38a09ae553
Barry <adebaresulaimon04@gmail.com>
2025-04-21 11:21:51 +0100

    testing app

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // Make sure to import the CSS
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chaos Mesh </h1>
+      <br />
+      <h2>🔥 "Injecting Chaos: Let’s Burn This Down (Safely)"</h2>
+    </div>
+  );
+}
+
+export default App;
